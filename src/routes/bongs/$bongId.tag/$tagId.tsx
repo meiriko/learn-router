edab34307bf322ce7eba194156dbdb7d81e8116a
Meir <meir.shahar@valerann.com>
2024-08-26 19:00:16 +0300

    tags prev/next and search params

--- src/routes/bongs/$bongId.tag/$tagId.tsx
+++ src/routes/bongs/$bongId.tag/$tagId.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@chakra-ui/react";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/bongs/$bongId/tag/$tagId")({
   component: TagItem,
@@ -7,9 +7,44 @@ export const Route = createFileRoute("/bongs/$bongId/tag/$tagId")({
 
 function TagItem() {
   const params = Route.useParams();
+  const search = Route.useSearch();
+  const tag = Number(params.tagId);
   return (
     <Box>
       <Box>Tag item with params {JSON.stringify(params)}</Box>
+      <Box>Tag item with search {JSON.stringify(search)}</Box>
+      <Box
+        px={2}
+        borderInlineStart="1px solid red"
+        as={Link}
+        from="/bongs/$bongId"
+        to="tag/$tagId"
+        params={{ tagId: tag - 1 }}
+        search={(prev: Record<string, string>) => ({
+          ...prev,
+          q: "coming-from-" + tag,
+          pp: "yes",
+        })}
+      >
+        goto prev tag
+      </Box>
+      <Box
+        px={2}
+        borderInlineStart="1px solid red"
+        as={Link}
+        from="/bongs/$bongId/tag"
+        to="$tagId"
+        params={{ tagId: tag + 1 }}
+        search={{
+          q: "left-behind-" + tag,
+          pn: "yes",
+          miro: ["x", "yy"],
+          shouldFinish: true,
+          jobj: { a: "b", c: { d: "e" } },
+        }}
+      >
+        goto next tag
+      </Box>
     </Box>
   );
 }

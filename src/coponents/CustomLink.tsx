d5812be2ad4c4c96fb6781afe68e1c767d45f289
Meir <meir@finaloop.com>
2024-08-29 16:38:39 +0300

    typing code cleanup

--- src/coponents/CustomLink.tsx
+++ src/coponents/CustomLink.tsx
@@ -9,100 +9,74 @@ import {
   LinkOptions as TanstackLinkOptions,
   RegisteredRouter,
   RoutePaths,
-  ParsePathParams,
-  ActiveLinkOptions,
+  // ParsePathParams,
+  // ActiveLinkOptions,
   // ToSubOptions,
   // ToOptions,
 } from "@tanstack/react-router";
-import React from "react";
 
-type ExtractFunctionType<T> = T extends (...args: any[]) => any ? T : never;
-type ToSearchType<
-  TTo extends RoutePaths<RegisteredRouter["routeTree"]> | undefined,
-> = ReturnType<
-  ExtractFunctionType<
-    TanstackLinkOptions<RegisteredRouter, "", TTo & string>["search"]
-  >
->;
+// type ExtractFunctionType<T> = T extends (...args: any[]) => any ? T : never;
+// type ToSearchType<
+//   TTo extends RoutePaths<RegisteredRouter["routeTree"]> | undefined,
+// > = ReturnType<
+//   ExtractFunctionType<
+//     TanstackLinkOptions<RegisteredRouter, "", TTo & string>["search"]
+//   >
+// >;
 
 // type RoutesTo = RoutePaths<RegisteredRouter["routeTree"]>;
 // const navLinks = ["/", "/about", "/items"] as RoutesTo[];
-// type ReplaceTypeByKeys<Type, Key extends keyof Type, ReplacedType> = Omit<
-//   Type,
-//   Key
-// > & { [K in Key]: ReplacedType };
-type ReplaceType<T, ReplacedType> = Omit<T, keyof T> & ReplacedType;
-type LinkChildren = TanstackLinkProps["children"];
-type LinkChildrenFN = Exclude<LinkChildren, React.ReactNode>;
 
-type AsNodeChildren<T> = ReplaceType<T, { children: React.ReactNode }>;
-type AsFunctionChildren<T> = ReplaceType<T, { children: LinkChildrenFN }>;
-// type AsNodeChildren<T extends { children: unknown }> = ReplaceTypeByKeys<
-//   T,
-//   "children",
-//   React.ReactNode
-// >;
-// type AsNodeChildren<T extends { children: LinkChildren }> = Omit<
-//   T,
-//   "children"
-// > & { children: React.ReactNode };
-// type AsFunctionChildren<T extends { children: LinkChildren }> = Omit<
-//   T,
-//   "children"
-// > & { children: LinkChildrenFN };
+// type ReplaceType<T, ReplacedType> = Omit<T, keyof T> & ReplacedType;
 
-type ToParamsType<
-  TTo extends RoutePaths<RegisteredRouter["routeTree"]> | undefined,
-> = TTo extends undefined
-  ? never
-  : ParsePathParams<TTo & string> extends never
-    ? never
-    : Record<ParsePathParams<TTo & string>, string>;
+// type AsNodeChildren<T> = ReplaceType<T, { children: React.ReactNode }>;
+// type AsFunctionChildren<T> = ReplaceType<T, { children: LinkChildrenFN }>;
 
-type ChakraToTanstackLinkProps<
-  TTo extends RoutePaths<RegisteredRouter["routeTree"]> | undefined,
-  // > = Omit<TanstackLinkOptions, "params"> &
-  // > = TanstackLinkProps<RegisteredRouter, "", TTo extends undefined ? "" : TTo> &
-> = TanstackLinkOptions<
-  RegisteredRouter,
-  "",
-  TTo extends undefined ? "" : TTo
-> &
-  ActiveLinkOptions &
-  // > = Omit<TanstackLinkProps, "params"> &
-  Omit<ChakraLinkProps, "as" | "children"> & {
-    // children: TanstackLinkProps["children"];
-    children: LinkChildren;
-    to?: TTo;
-    // params: ToParamsType<TTo>;
-  }; // & ToParamsType<TTo>;
+// type ToParamsType<
+//   TTo extends RoutePaths<RegisteredRouter["routeTree"]> | undefined,
+// > = TTo extends undefined
+//   ? never
+//   : ParsePathParams<TTo & string> extends never
+//     ? never
+//     : Record<ParsePathParams<TTo & string>, string>;
+
+// TanstackLinkProps &
+// Omit<ChakraLinkProps, "as" | "children"> & {
+//   children: LinkChildren;
+//   to?: TTo;
+//   // children: TanstackLinkProps["children"];
+//   // params: ToParamsType<TTo>;
+// };
+
+// & ToParamsType<TTo>;
 // } & (TTo extends undefined
 //   ? { params?: never }
 //   : ParsePathParams<TTo & string> extends never
 //     ? { params?: never }
 //     : { params: Record<ParsePathParams<TTo & string>, string> });
 
+type LinkChildren = TanstackLinkProps["children"];
+type LinkChildrenFN = Exclude<LinkChildren, React.ReactNode>;
+
+type ChakraToTanstackLinkProps<
+  TTo extends RoutePaths<RegisteredRouter["routeTree"]> | undefined,
+> = TanstackLinkOptions<
+  RegisteredRouter,
+  "",
+  TTo extends undefined ? "" : TTo
+> &
+  TanstackLinkProps &
+  Omit<ChakraLinkProps, "as" | "children">;
+
 export function CustomLink<
   TTo extends RoutePaths<RegisteredRouter["routeTree"]> | undefined,
 >(props: ChakraToTanstackLinkProps<TTo>) {
   if (typeof props.children === "function") {
-    const { children, ...rest } = props as AsFunctionChildren<
-      ChakraToTanstackLinkProps<TTo>
-    >;
+    const { children, ...rest } = props;
     return (
       <ChkraLinkBase
-        as={(props: TanstackLinkProps) => {
-          const { params, search, mask, ...rest } = props;
-          return (
-            <Box
-              {...rest}
-              mask={mask as any}
-              search={search as ToSearchType<TTo>}
-              params={params as ToParamsType<TTo>}
-              as={TanstackLink}
-              children={children}
-            />
-          );
+        as={(props: { children: LinkChildrenFN }) => {
+          return <Box {...props} as={TanstackLink} children={children} />;
         }}
         {...rest}
         paddingInlineEnd={2}
@@ -115,7 +89,7 @@ export function CustomLink<
     return (
       <ChkraLinkBase
         as={TanstackLink}
-        {...(props as AsNodeChildren<ChakraToTanstackLinkProps<TTo>>)}
+        {...(props as React.PropsWithChildren)}
         paddingInlineEnd={2}
         borderInlineEnd="1px solid red"
         whiteSpace="nowrap"
